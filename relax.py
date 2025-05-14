5bfc1f9be9a6d82c85d075482fa682646af1c8bd
DianaNtz <110336168+DianaNtz@users.noreply.github.com>
2025-05-14 12:05:11 +0200

    added code description and imported libraries

---
+++ relax.py
@@ -0,0 +1,8 @@
+"""
+The code below was written by https://github.com/DianaNtz and
+is an implementation of the Relaxation methods.
+"""
+
+#import libraries
+import numpy as np
+import matplotlib.pyplot as plt
